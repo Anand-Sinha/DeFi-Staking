4a7e10749809f667497b4b0a85012a2cf095a7a2
Anand Sinha <sinhaanand@icloud.com>
2021-10-02 17:54:27 +0530

    Updates

---
+++ precache-manifest.d680a59573490d151e2132ba6183bbbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a066daa02e892c34f470",
+    "url": "/DeFi-Staking/static/css/main.550fb51a.chunk.css"
+  },
+  {
+    "revision": "a066daa02e892c34f470",
+    "url": "/DeFi-Staking/static/js/main.a066daa0.chunk.js"
+  },
+  {
+    "revision": "6c1e621b4a90d6fb2e20",
+    "url": "/DeFi-Staking/static/css/1.b3d040ba.chunk.css"
+  },
+  {
+    "revision": "6c1e621b4a90d6fb2e20",
+    "url": "/DeFi-Staking/static/js/1.6c1e621b.chunk.js"
+  },
+  {
+    "revision": "0c9461706242d71b5f50",
+    "url": "/DeFi-Staking/static/js/runtime~main.0c946170.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/DeFi-Staking/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "1f83412545d002f5e70634a32fb742af",
+    "url": "/DeFi-Staking/index.html"
+  }
+];
